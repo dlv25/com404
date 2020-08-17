055fa8ad54510811863cd41ac20277b7c54925eb
dlv25 <3vennd69@solent.ac.uk>
2020-08-17 11:09:36 +0000

    Added code to read user iput

---
+++ 1-Basics/2-input/1-user-input/bot.py
@@ -0,0 +1,6 @@
+#Ask user to enter their name
+print("What is your name human?")
+#read in user's name
+name = input()
+#prints the message along with the user input
+print("It is nice to meet you human", name)
