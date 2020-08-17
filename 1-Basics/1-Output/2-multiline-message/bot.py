e5649c9c3a7b91d176d3be4967ccbf2b2a87ed81
dlv25 <3vennd69@solent.ac.uk>
2020-08-17 10:42:11 +0000

    Updated code to display a longer multi-line message.

--- 1-Basics/1-Output/2-multiline-message/bot.py
+++ 1-Basics/1-Output/2-multiline-message/bot.py
@@ -8,3 +8,13 @@ print("...rebooting hover engine")
 print()
 print("System Online.")
 #You can use a long string: print(""" """)
+print("""
+System Failure Imminent!
+System reboot has been initiated...
+
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+
+System online.
+""")
