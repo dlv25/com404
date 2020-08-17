9b45df3e3ec37422561244c072177d47322fc559
dlv25 <3vennd69@solent.ac.uk>
2020-08-17 11:20:33 +0000

    Added code to display an ascii robot with dynamic eyes.

---
+++ 1-Basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,10 @@
+#ask the user for an eye character
+print("Please enter a character for the eye:")
+eye = input ()
+
+#display an ascii art robot
+print("Beep's expression is now as follows:")
+print("##########")
+print("#  " + eye + "  " + eye + "  #")
+print("#  ----  #")
+print("##########")
