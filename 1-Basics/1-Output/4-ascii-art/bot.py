93c80bdee7e604ddd88fc6dbe29f080590972149
dlv25 <3vennd69@solent.ac.uk>
2020-08-17 10:51:24 +0000

    Added code to display an ascii art robot

---
+++ 1-Basics/1-Output/4-ascii-art/bot.py
@@ -0,0 +1,13 @@
+#Display an ascii art robot
+print("##########")
+print("#  0  0  #")
+print("#  ----  #")
+print("##########")
+
+#Display an ascii art robot using a long string
+print("""
+##########
+#  0  0  #
+#  ----  #
+########## 
+""")
